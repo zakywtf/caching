23afea390494c8c7831bd601880e5580e9a9f93d
nova zaky <novazaky1@gmail.com>
2020-03-17 14:31:21 +0700

    git init

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import express from 'express';
-import {countries, detailCountry} from './lib/cache';
-import {doParseHtml} from './lib/scraping';
+import bodyParser from "body-parser";
+import {getData, postData} from './lib/cache';
 import handleCtrl from './lib/ctrlHandler'
-import dotenv from 'dotenv'
 
-var app = express();  
-dotenv.config()
+var app = express(); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',async (req,res)=>{
     res.end('oke')
 })
 
-app.get('/corona/countries', async (req, res) => {
+app.get('/cache/get', async (req, res) => {
     handleCtrl(req, res, async (body) => {
-        return await countries()
+        return await getData()
     });
 })
 
-app.get('/corona/detail/:country', async (req, res) => {
+app.post('/cache/post', async (req, res) => {
     handleCtrl(req, res, async (body) => {
-        var {country} = req.params
-        return await detailCountry(country)
+        return await postData(req.body)
     });
 })
 
-var server = app.listen(process.env.PORT || 4321, function () { 
-    doParseHtml()
+var server = app.listen(process.env.PORT || 9876, function () { 
+    getData()
     var host = server.address().address  
     var port = server.address().port  
     console.log("Example app listening at http://%s:%s", host, port)  
--- src/lib/cache.js
+++ src/lib/cache.js
@@ -1,54 +1,15 @@
-import {getParseHtml} from './scraping';
+const CACHE=[]
 
-const COUNTRIES = []
-const DETAIL_COUNTRIES = {}
-
-const countries = async() => {
-    let array = await getParseHtml()
-    let arrayOfArray = array[0]
-    
-    for (let i = 0; i < arrayOfArray.length; i++) {
-        const arr = arrayOfArray[i];
-        var str = await replace(arr)
-        var projection = await getProjection(str)
-        COUNTRIES.push(projection)
-        DETAIL_COUNTRIES[`${projection.country}`] = projection
-    }
-    console.log({COUNTRIES, DETAIL_COUNTRIES});
-    
-    return COUNTRIES
+const getData = async() => {
+    return CACHE
 }
 
-const detailCountry = async(country) => {
-    return DETAIL_COUNTRIES[`${country}`]
-}
-
-const replace = (arr) => {
-    var newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        let element = arr[i];
-        let str = element.replace(/,/gi, "")
-        newArr.push(str)
-    }
-    return newArr
-}
-
-const getProjection = (arr) => {
-    return {
-        country:arr[0],
-        total_cases:parseInt(arr[1]) || 0, 
-        new_cases:parseInt(arr[3]) || 0, 
-        total_deaths:parseInt(arr[4]) || 0, 
-        new_deaths:parseInt(arr[5]) || 0,
-        total_recovered:parseInt(arr[6]) || 0,
-        active_cases:parseInt(arr[8]) || 0,
-        critical:parseInt(arr[9]) || 0,
-        top_cases:parseFloat(arr[11]) || 0
-        
-    }
+const postData = async(obj) => {
+    CACHE.push(obj)
+    return true
 }
 
 module.exports = {
-    countries,
-    detailCountry
+    getData,
+    postData
 }
